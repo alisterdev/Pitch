afa44205b286a62f03f514e4dc53193813a2997a
antonshevchenko <antonshevchenko@icloud.com>
2014-12-23 15:09:00 -0500

    Added initial search results list functionality.

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -1,6 +1,6 @@
 angular.module('app')
 
-.controller('SearchCtrl', function ($scope, PitchesResource) {
+.controller('SearchCtrl', function ($scope, $state, PitchesResource) {
 
   // Test data
   $scope.trending = [
@@ -15,6 +15,10 @@ angular.module('app')
     res.$httpPromise.then(function () {
       $scope.results = res;
     });
+  };
+
+  $scope.viewPitch = function (id) {
+    $state.go('tab.search-details', { id: id });
   };
 
 });
