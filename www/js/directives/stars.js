a067e07b6ab2e876b1dfe1c269f0cf738a44195b
antonshevchenko <antonshevchenko@icloud.com>
2014-12-23 00:37:43 -0500

    Improved stars directive.

--- www/js/directives/stars.js
+++ www/js/directives/stars.js
@@ -7,10 +7,13 @@ angular.module('app')
       repeat: '='
     },
     link: function (scope, el, attrs) {
+
       var star = '<i class="icon ion-ios-star"></i>'
         , halfStar = '<i class="icon ion-ios-star-half"></i>'
+        , emptyStar = '<i class="icon ion-ios-star-outline"></i>'
         , countStar = parseInt(scope.repeat)
-        , countHalfStar = scope.repeat % 2;
+        , countHalfStar = scope.repeat - countStar
+        , countEmptyStar = parseInt(5 - scope.repeat);
 
       for (var i = 0; i < countStar; i++) {
         el.append(star);
@@ -19,6 +22,11 @@ angular.module('app')
       if (countHalfStar !== 0) {
         el.append(halfStar);
       }
+
+      for (var i = 0; i < countEmptyStar; i++) {
+        el.append(emptyStar);
+      }
+
     }
   };
 });
