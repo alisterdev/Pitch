ea6a47d356e9e8ca6fcb5421a8343a1db1ad3438
antonshevchenko <antonshevchenko@icloud.com>
2014-12-25 01:47:09 -0500

    Added initial description modal for create pitch view.

--- www/js/controllers/pitch.js
+++ www/js/controllers/pitch.js
@@ -1,6 +1,6 @@
 angular.module('app')
 
-.controller('PitchCtrl', function ($scope, CategoriesResource, UserService) {
+.controller('PitchCtrl', function ($scope, $ionicModal, CategoriesResource, UserService) {
 
   function getCategories () {
     var res = CategoriesResource.query();
@@ -20,6 +20,25 @@ angular.module('app')
     pitch['creator'] = user._id;
 
     console.log(pitch);
+  };
+
+  // Description modal
+  $scope.showModalDescription = function () {
+    if (typeof $scope.modalDescription === 'undefined') {
+      $ionicModal.fromTemplateUrl('description.html', {
+        scope: $scope,
+        animation: 'slide-right-left'
+      }).then(function (modal) {
+        $scope.modalDescription = modal;
+        $scope.modalDescription.show();
+      });
+    } else {
+      $scope.modalDescription.show();
+    }
+  };
+
+  $scope.hideModalDescription = function () {
+    $scope.modalDescription.hide();
   };
 
   getCategories();
