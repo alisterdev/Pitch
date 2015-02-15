cba34c7c4d570e098651baa58303412b83f3a469
antonshevchenko <antonshevchenko@icloud.com>
2015-02-15 12:12:56 -0500

    Added pitch status codes.

--- app/js/constants.js
+++ app/js/constants.js
@@ -9,4 +9,12 @@ angular.module('app')
     none: 'user.none'
   },
   unauthorized: 'user.unauthorized'
+})
+
+// Pitch Status Codes
+.constant('PITCH_STATUS', {
+  0: 'active',
+  1: 'completed',
+  2: 'cancelled',
+  3: 'deleted'
 });
